53614c7532830ba961840044253ca1f7b5876366
jakeTran42 <jake.tranehs@gmail.com>
2018-10-13 16:04:42 -0700

    final project

---
+++ FEW1.2/phaser3-project/src/index.js
@@ -0,0 +1,51 @@
+import 'phaser';
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+// function preload ()
+// {
+//     this.load.image('logo', 'assets/Rimuru_2.png');
+// }
+
+// function create ()
+// {
+//     var logo = this.add.image(400, 150, 'logo');
+
+//     this.tweens.add({
+//         targets: logo,
+//         y: 450,
+//         duration: 5000,
+//         ease: 'Power2',
+//         yoyo: true,
+//         loop: -1
+//     });
+
+// }
+
+
+function preload ()
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+function create () {
+    this.add.image(400, 300, 'sky');
+}
