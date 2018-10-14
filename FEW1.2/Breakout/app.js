60dc006603c4608ff079b9a44ec59d622f674643
Jake Tran <jake.tranehs@gmail.com>
2018-10-14 05:49:28 -0700

    removed fillers

--- FEW1.2/Breakout/app.js
+++ FEW1.2/Breakout/app.js
@@ -33,5 +33,35 @@ class Ball {
     }
 }
 
+class Brick {
+    constructor(x = 0, y = 0, status = 1, width = 75, height = 20, color = getAColor('dark')) {
+        this.x = x;
+        this.y = y;
+        this.status = status;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    render(ctx) {
+        // Set locations for gradient
+        const startX = this.x + this.width / 2;
+        const startY = this.y + this.width;
+        const endX = this.x + this.width / 2;
+        const endY = this.y;
+        // Create gradient
+        const brkGrd = ctx.createLinearGradient(startX, startY, endX, endY);
+        // Places a color at the start
+        brkGrd.addColorStop(0, 'black');
+        // Places a color at the end
+        brkGrd.addColorStop(1, this.color);
+        // Paint gradient
+        ctx.beginPath();
+        ctx.fillStyle = brkGrd;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.closePath();
+
+    }
+}
+
 
 draw();
