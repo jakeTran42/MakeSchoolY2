ab21796cd3f13b7d0b314315547de301a1fd22f3
Jake Tran <jake.tranehs@gmail.com>
2018-10-18 05:50:05 -0700

    removed fillers

--- FEW1.2/Breakout/app.js
+++ FEW1.2/Breakout/app.js
@@ -63,6 +63,23 @@ class Brick {
     }
 }
 
+class Paddle {
+    constructor(x = (canvas.width - 75) / 2, y = 0, width = 75, height = 10, color = getAColor('dark')) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    render(ctx) {
+        ctx.beginPath();
+        ctx.rect(this.x, canvas.height - this.height, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
 const brickRowCount = 5;
 const brickColumnCount = 6;
 const brickPadding = 10;
