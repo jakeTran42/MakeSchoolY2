832a5921d35193dcf5a61684c28ea0a529223013
Jake Tran <jake.tranehs@gmail.com>
2018-11-13 05:10:36 -0800

    refactored

--- FEW1.2/phaser3-project/src/index.js
+++ FEW1.2/phaser3-project/src/index.js
@@ -175,3 +175,31 @@ function update ()
         player.setVelocityY(-330);
     }
 }
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    //  Add and update the score
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        //  A new batch of stars to collect
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    }
+}
