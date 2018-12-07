53b4af6b840bfc6f1d94cecc16a13ef0a59df975
Jake Tran <jake.tranehs@gmail.com>
2018-12-07 03:23:35 -0800

    add new resolver

--- FEW2.9/hackernews-node/src/index.js
+++ FEW2.9/hackernews-node/src/index.js
@@ -1,19 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
 
 // 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-
-`
 
 let links =[{
   id: 'link-0',
@@ -21,24 +8,33 @@ let links =[{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length;
+
 // 2
 const resolvers = {
-  
+
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
   },
 
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url
-  }
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
