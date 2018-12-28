5a9dd91e85ce185c797f8b9b8ae87bf8c7126c43
jakeTran42 <jake.tranehs@gmail.com>
2018-12-28 12:51:28 -0800

    39

--- FEW2.9/hackernews-node/src/index.js
+++ FEW2.9/hackernews-node/src/index.js
@@ -5,6 +5,7 @@ const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
 const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 // let links =[{
 //   id: 'link-0',
@@ -20,7 +21,8 @@ const resolvers = {
     Mutation,
     Subscription,
     User,
-    Link
+    Link,
+    Vote,
 }
 
 // 3
